91acd4c3efa2e4d5dd81f4f90e2089a0cd809cb5
VictorLemosbar <victor.escolas9045@gmail.com>
2026-01-17 20:07:56 -0300

    media query adicionado no layout,jsx e layout.css

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -5,7 +5,7 @@ const Layout = ({children}) => {
 
     return (
         <>
-                {children}                
+                <div className="app-container">{children}</div>                
         </>
     )
 }
